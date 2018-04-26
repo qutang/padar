2686d50d41d1161dc9e2d6db47829b5becc186fb
qutang <tqshelly@gmail.com>
2018-04-25 21:38:53 -0400

    Update version code

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='padar',
-    version='1.0.1',
+    version='1.0.2',
     description='Processing Accelerometer Data and Do Activity Recognition',
     long_description_content_type='text/markdown',
     url='https://github.com/qutang/padar',
