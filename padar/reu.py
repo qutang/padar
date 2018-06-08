218e2144e182206e18438287b674de406417630e
qutang <tqshelly@gmail.com>
2018-06-08 16:32:02 -0400

    Add more models for testing

---
+++ padar/reu.py
@@ -0,0 +1,80 @@
+import click
+import pandas as pd
+from .api import M
+import os
+import warnings
+import numpy
+import importlib
+import sys
+from .scripts.multilocation_2017 import FeatureSetPreparer
+from .scripts.models.activity_intensity import ActivityIntensityClassifier
+from .scripts.models.posture import PostureClassifier
+from .scripts.models.daily_activity import DailyActivityClassifier
+from .scripts.models.sedentary_ambulation import SedentaryAmbulationClassifier
+from .scripts.models.hand_gesture import HandGestureClassifier
+from .scripts.models.MDCAS import MDCASClassifier
+
+@click.group()
+@click.pass_context
+def main(ctx):
+    """[summary]
+    
+    [description]
+    """
+    ctx.obj={}
+
+@click.command(context_settings=dict(
+    ignore_unknown_options=True,
+    allow_extra_args=True
+))
+@click.argument('input_file')
+@click.option('--output_file', '-o', help='output file path')
+@click.pass_context
+def compute_features(ctx, input_file, output_file):
+    """
+        Function to compute features for MDCAS classifier
+    """
+    kwargs = {ctx.args[i][2:]: ctx.args[i+1].strip('"') for i in range(0, len(ctx.args), 2)}
+    output_file = os.path.abspath(output_file)
+    click.echo("Init feature set computation")
+    executor = FeatureSetPreparer.build(verbose=True, violate=True, independent=True, session_file=None, location_mapping_file = None, orientation_fix_file=None, ws=12800, ss=12800, threshold=0.2, subwins=4, skip_post=True, **kwargs)
+    click.echo("Compute feautures")
+    result = executor(input_file)
+    if not os.path.exists(os.path.dirname(output_file)):
+        click.echo("Create output folder if not exists")
+        os.makedirs(os.path.dirname(output_file))
+    click.echo("Save feature set to: " + output_file)
+    result.to_csv(output_file, index=False, float_format='%.6f')
+    click.echo("Saved")
+
+@click.command(context_settings=dict(
+    ignore_unknown_options=True,
+    allow_extra_args=True
+))
+@click.argument('input_file')
+@click.option('--model', '-m', help='model bundle file')
+@click.option('--output_file', '-o', help='output file path in csv')
+@click.pass_context
+def test(ctx, input_file, model, output_file):
+    """
+        Run test on data using the four different classifiers
+    """
+    # parse extra input args
+    kwargs = {ctx.args[i][2:]: ctx.args[i+1].strip('"') for i in range(0, len(ctx.args), 2)}
+    if 'use_groups' in kwargs:
+        if kwargs['use_groups']:
+            no_groups = 0
+        else:
+            no_groups = 1
+    else:
+        no_groups = 1
+    click.echo('Init model from: ' + model)
+    model_class = MDCASClassifier.init(True, None, None)
+    click.echo('Make prediction on: ' + input_file)
+    pred_df = model_class.test(model_bundle_file = model, test_set_file=input_file, gt_set_file=None, input_format='joblib', verbose=True, prob=1, no_groups=no_groups)
+    click.echo('Save predictions to: ' + output_file)
+    model_class.export_test(output_file)
+    click.echo('Saved')
+
+main.add_command(compute_features)
+main.add_command(test)
---
+++ padar/scripts/models/daily_activity/DailyActivityClassifier.py
@@ -0,0 +1,164 @@
+from ..BaseModel import BaseModel
+from ....api import utils as mu
+import numpy as np
+from sklearn import preprocessing
+import sklearn.svm as svm
+from sklearn.utils import shuffle
+from sklearn.model_selection import RandomizedSearchCV
+import scipy
+
+def init(verbose, feature_set, class_set):
+    return PostureClassifier(verbose, feature_set, class_set)
+
+class DailyActivityClassifier(BaseModel):
+    def __init__(self, verbose, feature_set, class_set):
+        BaseModel.__init__(self, verbose, feature_set, class_set)
+        self._name = "daily activity classifier"
+        self._description = "AR model trained on acceleromete data from multiple sensors to classify 14 daily activities"
+
+    def _preprocess(self, train_df, class_df):
+        if train_df.shape[0] != class_df.shape[0]:
+            raise ValueError("Training data should have the same amount with class labels")
+        if np.any(train_df.iloc[:,0].values != class_df.iloc[:,0].values):
+            raise ValueError("Training data and class labels should be sorted and have exactly the same windows")
+
+        # clean up dataset
+        train_set = train_df.drop(labels = train_df.columns[0:2].tolist() + ['pid', 'sid', 'location'], axis=1).values
+        class_set = class_df.loc[:, "activity"].values
+
+        # preprocess traing set
+        # 1. standardize
+        self._scaler = preprocessing.MinMaxScaler((-1, 1))
+        train_set = self._scaler.fit_transform(train_set)
+        
+        # preprocess class set
+        # 1. discard unknown and transition
+        unknown_mask = np.logical_or(class_set == 'unknown', class_set == 'transition')
+        class_set = class_set[np.logical_not(unknown_mask)]
+        train_set = train_set[np.logical_not(unknown_mask),:]
+
+        # randomize
+        shuffled_train_set, shuffled_class_set = shuffle(train_set, class_set)
+
+
+        if shuffled_train_set.shape[0] != shuffled_class_set.shape[0]:
+            raise ValueError("Training data should have the same amount with class labels")
+
+        return shuffled_train_set, shuffled_class_set
+
+    def _preprocess_test(self, test_df, **kwargs):
+        if kwargs['no_groups']:
+            df = test_df.copy(deep=True)
+        else:
+            df = test_df.copy(deep=True).drop(labels=['pid', 'sid', 'location'], axis=1)
+        df = df.dropna()
+        test_df = test_df.iloc[df.index,:]
+        if kwargs['no_groups']:
+            labels = test_df.columns[0:2].tolist()
+        else:
+            labels = test_df.columns[0:2].tolist() + ['pid', 'sid', 'location']
+        test_set = test_df.drop(labels = labels, axis=1).values
+        # preprocess test set
+        # 1. standardize
+        test_set = self._scaler.transform(test_set)
+        return df.index, test_set
+
+    def _cv(self, n_folds=10, n_repeats=5):
+        return self 
+
+    def _train(self, **kwargs):
+        '''
+        '''
+        train_df = self._feature_set.copy(deep=True)
+        class_df = self._class_set.copy(deep=True)
+
+        shuffled_train_set, shuffled_class_set = self._preprocess(train_df, class_df)        
+
+        print(shuffled_train_set[0:10,:])
+        print(shuffled_class_set[0:10])
+
+        self._paras = {
+            'C': 262.48813329580832, 
+            'class_weight': None, 
+            'gamma': 0.17399718482493676, 
+            'kernel': 'rbf', 
+            'tol': 1e-05,
+            'verbose': True,
+            'probability': True
+        }
+        if kwargs['hyper_search'] in ['True', '1']:
+            tuned_parameters = {
+                'C': scipy.stats.expon(scale=100), 
+                'gamma': scipy.stats.expon(scale=.1),
+                'kernel': ['rbf'], 
+                'class_weight':['balanced', None],
+                'tol': [0.00001]
+            }
+
+            clf = svm.SVC()
+
+            scoring = 'f1_macro'
+
+            hyper_clfs = RandomizedSearchCV(clf, tuned_parameters, cv=5, n_iter=20, scoring=scoring, n_jobs=7, verbose=3, refit=False)
+
+            hyper_clfs.fit(shuffled_train_set, shuffled_class_set)
+
+            print("# Tuning hyper-parameters for %s" % scoring)
+            print()
+
+            print("Best parameters set found on development set:")
+            print()
+            print(hyper_clfs.best_params_)
+            print()
+            print("Scores on development set:")
+            print()
+            means = hyper_clfs.cv_results_['mean_test_score']
+            stds = hyper_clfs.cv_results_['std_test_score']
+            for mean, std, params in zip(means, stds, hyper_clfs.cv_results_['params']):
+                print("%0.3f (+/-%0.03f) for %r"
+                    % (mean, std * 2, params))
+            print()
+            self._paras = hyper_clfs.best_params_
+
+        best_clf = svm.SVC(**self._paras)
+        best_clf.fit(shuffled_train_set, shuffled_class_set)
+        self._trained_model = best_clf
+
+        self._bundle = {
+            'model': best_clf,
+            'paras': self._paras,
+            'scaler': self._scaler,
+            'train_set_file': self._feature_set_file,
+            'class_set_file': self._class_set_file
+        }
+
+        return self
+        
+    def _test(self, test_df, **kwargs):
+        nonnull_indices, test_set = self._preprocess_test(test_df, no_groups=kwargs['no_groups'])
+        
+        labels = self._trained_model.classes_
+        
+        if kwargs['prob'] in ['True', '1', 1, True]:
+            pred_prob_set = self._trained_model.predict_proba(test_set)
+            pred_max_prob_set = np.max(pred_prob_set, axis=1)
+            pred_set_label_indices = np.argmax(pred_prob_set, axis=1)
+            pred_set = labels[pred_set_label_indices]
+        else:
+            pred_set = self._trained_model.predict(test_set)
+        if kwargs['no_groups']:
+            cols = test_df.columns[0:2].tolist()
+        else:
+            cols = test_df.columns[0:2].tolist() + ['pid', 'sid', 'location']
+        pred_df = test_df[cols]
+        pred_df = pred_df.iloc[nonnull_indices,:]
+        pred_df['DAILY_ACTIVITY_PREDICTION'] = pred_set
+        if kwargs['prob'] in ['True', '1', 1, True]:
+            pred_df['DAILY_ACTIVITY_PREDICTION_PROB'] = pred_max_prob_set
+        self._pred_df = pred_df
+        return self
+        
+
+        
+        
+        
